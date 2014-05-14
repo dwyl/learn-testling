df1dd243de6bf46fe95c1b8fc53b3745a9732293
nelsonic <nodecoder@gmail.com>
2014-05-14 22:27:14 +0100

    attempt using mocha for testling tests - https://ci.testling.com/guide/mocha

--- test/max.js
+++ test/max.js
@@ -1,9 +1,20 @@
-var maxBy = require('../');
-var test = require('tape');
+var chai = require('chai');
+var assert = chai.assert; 
 
-test('simple comparisons', function (t) {
-    t.plan(1);
-
-    var n = maxBy([9,3,4], function (x) { return x % 3 });
-    t.equal(n, 4);
+describe('Cash Register', function(){
+	it('should have a getChange Method', function(){
+		assert.equal(1,1);
+	});
 });
+
+// var maxBy = require('../');
+// var test = require('tape');
+
+// test('simple comparisons', function (t) {
+//     t.plan(1);
+
+//     var n = maxBy([9,3,4], function (x) {
+	//     return x % 3
+//     });
+//     t.equal(n, 4);
+// });
