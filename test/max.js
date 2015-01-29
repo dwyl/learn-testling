50bfb9b65e85b8c3de5c8a205d50d6ce7899e25e
nelsonic <contact.nelsonic@gmail.com>
2015-01-29 20:49:08 +0000

    rever back to tape for tests

--- test/max.js
+++ test/max.js
@@ -1,24 +1,24 @@
-var assert = require('assert');
-var maxBy = require('../');
-
-suite('Mocha TDD simple comparisons', function(){
-	test('maxBy [9,3,4] should return 4', function(){
-		var n = maxBy([9,3,4], function (x) {
-			return x % 3;
-		});
-		assert.equal(n, 4);
-	});
-});
+// var assert = require('assert');
+// var maxBy = require('../');
+//
+// suite('Mocha TDD simple comparisons', function(){
+// 	test('maxBy [9,3,4] should return 4', function(){
+// 		var n = maxBy([9,3,4], function (x) {
+// 			return x % 3;
+// 		});
+// 		assert.equal(n, 4);
+// 	});
+// });
 
 /*** IGNORE - Old tape tests for reference only ***/
-// var maxBy = require('../');
-// var test = require('tape');
+var maxBy = require('../');
+var test = require('tape');
 
-// test('simple comparisons', function (t) {
-//     t.plan(1);
+test('simple comparisons', function (t) {
+    t.plan(1);
 
-//     var n = maxBy([9,3,4], function (x) {
-	//     return x % 3
-//     });
-//     t.equal(n, 4);
-// });
+    var n = maxBy([9,3,4], function (x) {
+	    return x % 3
+    });
+    t.equal(n, 4);
+});
